1d6ec7597444b1eeb9844cbd4a820da6a585fb09
Lara Moreira <up202306822@edu.fe.up.pt>
2025-03-06 12:37:42 +0000

    Initial Graph Class

--- graph.h
+++ graph.h
@@ -1,16 +1,161 @@
-//
-// Created by Vasco Lemos on 06/03/2025.
-//
+// Original code by Gonçalo Leão
+// Updated by DA 2024/2025 Team
 
-#ifndef GRAPH_H
-#define GRAPH_H
+#ifndef DA_TP_CLASSES_GRAPH
+#define DA_TP_CLASSES_GRAPH
+
+#include <iostream>
+#include <vector>
 
 
+template <class T>
+class Edge;
 
-class graph {
+#define INF std::numeric_limits<double>::max()
+
+/************************* Vertex  **************************/
+
+template <class T>
+class Vertex {
+public:
+    Vertex(T in);
+    bool operator<(Vertex<T> & vertex) const; // // required by MutablePriorityQueue
+
+    T getInfo() const;
+    std::vector<Edge<T> *> getAdj() const;
+    bool isVisited() const;
+    bool isProcessing() const;
+    unsigned int getIndegree() const;
+    double getDist() const;
+    Edge<T> *getPath() const;
+    std::vector<Edge<T> *> getIncoming() const;
+
+    void setInfo(T info);
+    void setVisited(bool visited);
+    void setProcessing(bool processing);
+
+    int getLow() const;
+    void setLow(int value);
+    int getNum() const;
+    void setNum(int value);
+
+    void setIndegree(unsigned int indegree);
+    void setDist(double dist);
+    void setPath(Edge<T> *path);
+    Edge<T> * addEdge(Vertex<T> *dest, double w);
+    bool removeEdge(T in);
+    void removeOutgoingEdges();
+
+    //Content added
+    bool isPark();
+    void setPark(bool park);
+
+protected:
+    T info;                // info node
+    std::vector<Edge<T> *> adj;  // outgoing edges
+
+    // auxiliary fields
+    bool visited = false; // used by DFS, BFS, Prim ...
+    bool processing = false; // used by isDAG (in addition to the visited attribute)
+    int low = -1, num = -1; // used by SCC Tarjan
+    unsigned int indegree; // used by topsort
+    double dist = 0;
+    Edge<T> *path = nullptr;
+
+    std::vector<Edge<T> *> incoming; // incoming edges
+
+    int queueIndex = 0; 		// required by MutablePriorityQueue and UFDS
+
+    void deleteEdge(Edge<T> *edge);
+
+    //Content added
+    bool park = false;
+};
+
+/********************** Edge  ****************************/
+
+template <class T>
+class Edge {
+public:
+    Edge(Vertex<T> *orig, Vertex<T> *dest, double w);
+
+    Vertex<T> * getDest() const;
+    double getWeight() const;
+    bool isSelected() const;
+    Vertex<T> * getOrig() const;
+    Edge<T> *getReverse() const;
+    double getFlow() const;
+
+    void setSelected(bool selected);
+    void setReverse(Edge<T> *reverse);
+    void setFlow(double flow);
+
+    //Content Added
+    void setWalk(double walk);
+    double getWalk();
+    void setDrive(double drive);
+    double getDrive();
+
+protected:
+    Vertex<T> * dest; // destination vertex
+    double weight; // edge weight, can also be used for capacity
+
+    // auxiliary fields
+    bool selected = false;
+
+    // used for bidirectional edges
+    Vertex<T> *orig;
+    Edge<T> *reverse = nullptr;
+
+    //Content Added
+    double drive, walk;
 
 };
 
+/********************** Graph  ****************************/
+
+template <class T>
+class Graph {
+public:
+    ~Graph();
+    /*
+    * Auxiliary function to find a vertex with a given the content.
+    */
+    Vertex<T> *findVertex(const T &in) const;
+    /*
+     *  Adds a vertex with a given content or info (in) to a graph (this).
+     *  Returns true if successful, and false if a vertex with that content already exists.
+     */
+    bool addVertex(const T &in);
+    bool removeVertex(const T &in);
+
+    /*
+     * Adds an edge to a graph (this), given the contents of the source and
+     * destination vertices and the edge weight (w).
+     * Returns true if successful, and false if the source or destination vertex does not exist.
+     */
+    bool addEdge(const T &sourc, const T &dest, double w);
+    bool removeEdge(const T &source, const T &dest);
+    bool addBidirectionalEdge(const T &sourc, const T &dest, double w);
+
+    int getNumVertex() const;
+    std::vector<Vertex<T> *> getVertexSet() const;
+
+protected:
+    std::vector<Vertex<T> *> vertexSet;    // vertex set
+
+    double ** distMatrix = nullptr;   // dist matrix for Floyd-Warshall
+    int **pathMatrix = nullptr;   // path matrix for Floyd-Warshall
+
+    /*
+     * Finds the index of the vertex with a given content.
+     */
+    int findVertexIdx(const T &in) const;
+};
+
+void deleteMatrix(int **m, int n);
+void deleteMatrix(double **m, int n);
 
 
-#endif //GRAPH_H
+
+#endif /* DA_TP_CLASSES_GRAPH */
